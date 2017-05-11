dde28c3428b7af8d5378380a344e464f6686e78b
carlo <carlo@DESKTOP-1J8P3O3>
2017-05-11 16:29:34 -0600

    bibliotecas

--- src/Clases/RegistroClase.java
+++ src/Clases/RegistroClase.java
@@ -6,7 +6,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import javafx.scene.control.ComboBox;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 /**
